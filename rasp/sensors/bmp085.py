3de63445b7857d6e2bc5d2edb0bd7d0b3ae27eba
Jean-Manuel Gagnon <jean_manu@icloud.com>
2016-01-02 12:57:51 -0500

    testet and working but without error state
    support to BMP085 sensor for altitude and Cockpit air temperature

--- rasp/sensors/bmp085.py
+++ rasp/sensors/bmp085.py
@@ -1,14 +1,16 @@
+#!/usr/bin/python
+
 import sensor
 import bmpBackend
 
 class BMP085(sensor.Sensor):
 	bmpClass = None
-	requiredData = ["measurement","i2cbus"]
+	requiredData = ["measurement"]
 	optionalData = ["pressure","altitude","mslp","unit"]
 	def __init__(self,data):
 		self.sensorName = "BMP085"
 		if "temp" in data["measurement"].lower():
-			self.valName = "Temperature"
+			self.valName = "CAT"
 			self.valUnit = "Celsius"
 			self.valSymbol = "C"
 			if "unit" in data:
@@ -33,31 +35,42 @@ class BMP085(sensor.Sensor):
                         self.valName = "Altitude"
 			self.valSymbol = ""
 			self.valUnit = "Feet"
-			self.pressure=data["pressure"]
                         if "unit" in data:
 				if data["unit"].lower in ["meter","Meter","M","m"]:
 					self.valUnit = "Meter"
 					self.valSymbol = "M"
 			
 		if (BMP085.bmpClass==None):
-			BMP085.bmpClass = bmpBackend.BMP085(bus=int(data["i2cbus"]))
-		return
+                        BMP085.bmpClass = bmpBackend.BMP085()
+                return
 
 	def getVal(self):
-		if self.valName == "Temperature":
-			temp = BMP085.bmpClass.readTemperature()
+		if self.valName == "CAT":
+                        try:
+                                temp = BMP085.bmpClass.read_temperature()
+                        except:
+                                temp = -50
+                                pass
 			if self.valUnit == "Fahrenheit":
 				temp = temp * 1.8 + 32
 			return temp
 		elif self.valName == "Pressure":
-			if self.mslp:
-				return BMP085.bmpClass.readMSLPressure(self.altitude) * 0.01 #to convert to Hectopascals
-			else:
-				return BMP085.bmpClass.readPressure() * 0.01 #to convert to Hectopascals
+                        try:
+                                if self.mslp:
+                                        return BMP085.bmpClass.read_sealevel_pressure(self.altitude) * 0.01 #to convert to Hectopascals
+                                else:
+                                        return BMP085.bmpClass.read_pressure() * 0.01 #to convert to Hectopascals
+                        except:
+                                return 0
+                        return
 		elif self.valName == "Altitude":
-                        alt = BMP085.bmpClass.readAltitude(self.pressure)
+                        try:
+                                alt = BMP085.bmpClass.read_altitude()
+                        except:
+                                alt = 15000
+                                pass
                         if self.valUnit == "Feet":
 				alt = round(alt * 3.28084)
-			return int(alt)	
+                        return int(alt)	
 
 			
