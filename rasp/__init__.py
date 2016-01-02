3c2670e0618987552bc3d3a005783887e29437f0
Jean-Manuel Gagnon <jean_manu@icloud.com>
2016-01-02 12:34:15 -0500

    add new virtual wire thread and implementation of other sensors
    implementation of new sensor and add a new thread for virtual wire
    (remote arduino pro, indicated air speed sensor, outside air
    temperature sensor and 433mhz transmitter) with 433mhz receiver on pi

--- rasp/__init__.py
+++ rasp/__init__.py
@@ -1,7 +1,7 @@
 #This file takes in inputs from a variety of sensor files , and outputs information
 import sys
 sys.dont_write_bytecode = True
-import numpy as np
+#import numpy as np
 import RPi.GPIO as GPIO
 import ConfigParser
 import time
@@ -10,7 +10,66 @@ import os
 from sys import exit
 import threading
 from rasp.sensors import sensor
+from collections import deque
 
+class VW(threading.Thread):
+
+    def __init__(self, conn):
+        threading.Thread.__init__(self)
+        self.queue = conn
+        self.ias = 0
+        self.oat = 0
+        self.ias_W_S = 0
+        self.oat_W_S = 0
+        if not os.path.isfile('/home/pi/pyEfis/rasp/virtualwire.cfg'):
+                        print "Unable to access config file: virtualwire.cfg"
+                        exit(1)
+        sensorConfig = ConfigParser.SafeConfigParser()
+        sensorConfig.read('/home/pi/pyEfis/rasp/virtualwire.cfg')
+        i="VirtualWire"
+        sensorNames = sensorConfig.sections()
+        if "VirtualWire" in sensorNames:
+            try:
+                enabled = sensorConfig.getboolean(i,"enabled")
+            except Exception:
+                enabled = True
+        if enabled:
+            from rasp.sensors import vwBackend
+            import pigpio
+            rxpin = int(sensorConfig.get(i,"rxpin"))
+            bps = int(sensorConfig.get(i,"bps"))
+            pigpio.exceptions = False
+            self.pi = pigpio.pi() # Connect to local Pi
+            self.rx = vwBackend.rx(self.pi, rxpin, bps) # Specify Pi, rx GPIO.
+            self.running = 1
+
+    def run(self):
+        print "Virtualwire thread started"
+        while self.running:
+            time.sleep(0.6)
+            try:
+                while self.rx.ready():
+                    msg = str("".join(chr (c) for c in self.rx.get()))
+                msg = msg.split(',')
+                if float(msg[0]) < 400 :
+                    self.ias = float(msg[0])
+                    self.ias_W_S = 3
+                if float(msg[1]) > -50 :                   
+                    self.oat = float(msg[1])
+                    self.oat_W_S = 3
+                else:
+                    self.ias_W_S = 2
+                    self.oat_W_S = 2
+            except:
+                pass
+            data = [self.ias, self.oat, self.ias_W_S, self.oat_W_S]
+            data_test = str(data).strip('[]')
+            self.queue.put(data_test)
+    
+    def stop(self):
+        self.rx.cancel()
+        self.pi.stop()
+        self.running = 0
 
 class GPIO_Process(threading.Thread):
 
@@ -19,21 +78,23 @@ class GPIO_Process(threading.Thread):
         threading.Thread.__init__(self)
         self.queue = conn
         self.hg = 29.92
-        self.speed = 34
-        self.pitch = 15
-        self.roll = 15
-        self.heading = 180
+        self.ias = 0
+        self.pitch = 0
+        self.roll = 0
+        self.heading = 0
         self.alt = 0
         self.co = 0
         self.volt = 12
         self.cat = 0
-        self.alt_W_S = 2
-        self.cat_W_S = 2
-        self.gyro_W_S = 2
-        self.mag_W_S = 2
-        self.ias_W_S = 2
-        self.oat_W_S = 2
-
+        self.oat = 0
+        self.alt_W_S = 0
+        self.cat_W_S = 0
+        self.gyro_W_S = 0
+        self.heading_W_S = 0
+        self.ias_W_S = 0
+        self.oat_W_S = 0
+        self.smooted = 0.7
+       
         def get_subclasses(mod,cls):
                 for name, obj in inspect.getmembers(mod):
                         if hasattr(obj, "__bases__") and cls in obj.__bases__:
@@ -107,14 +168,13 @@ class GPIO_Process(threading.Thread):
                                 print ("Success: Loaded sensor plugin " + i)
                 except Exception as e: #add specific exception for missing module
                         print("Error: Did not import sensor plugin " + i )
-                        raise e
+                        #raise e
         self.running = 1
 
-                
+
     def run(self):
-        def runningMean(x, N):
-                    return np.convolve(x, np.ones((N,))/N)[(N-1):]
-        cat_sample = 0                               
+        cat_sample = 0    
+        oat_sample = 0                               
         while self.running:                    
                 data = []
                 #Collect the data from each sensor
@@ -123,41 +183,68 @@ class GPIO_Process(threading.Thread):
                         val = i.getVal()
                         if val==None: #this means it has no data to upload.
                                 continue
-                        #dataDict["value"] = i.getVal()
-                        dataDict["unit"] = i.valUnit
-                        dataDict["symbol"] = i.valSymbol
                         dataDict["name"] = i.valName
                         dataDict["sensor"] = i.sensorName
-                        if dataDict["name"] == "Speed":
-                                self.speed = i.getVal()
-                        elif dataDict["name"] == "Pitch":
-                                self.pitch = i.getVal()
-                        elif dataDict["name"] == "Roll":
-                                self.roll = i.getVal()
-                        elif dataDict["name"] == "Heading":
-                                self.heading = i.getVal()
+                        if dataDict["name"] == "IAS":
+                            try:
+                                ias_Value = i.getVal()
+                                if self.ias < 200:
+                                    self.ias = int((self.alt*self.smooted)+(1.0-self.smooted)*(ias_Value))
+                                    self.ias_W_S = 3
+                                elif  not (200 <= self.ias >= 250):
+                                    self.ias_W_S = 1
+                                else:
+                                    self.ias = 0
+                                    self.ias_W_S = 2
+                            except:
+                                pass
+                        elif dataDict["name"] == "Orientation":
+                                msg = i.getVal()
+                                msg = str(msg).strip('[]')
+                                msg = msg.split(',')
+                                if int(msg[0]) != 1000:
+                                    self.heading = int(msg[0])
+                                    self.roll = int(msg[1])
+                                    self.pitch = int(msg[2])
+                                    self.heading_W_S = 3
+                                    self.roll_W_S = 3
+                                    self.pitch_W_S = 3
+                                else:
+                                    self.heading_W_S = 2
+                                    self.roll_W_S = 2
+                                    self.pitch_W_S = 2
+                                #print msg
                         elif dataDict["name"] == "Altitude":
                             try:
                                 alt_Value = i.getVal()
                                 if alt_Value < 10000:
-                                    self.alt = int(runningMean(alt_Value, 3))
+                                    self.alt = int((self.alt*self.smooted)+(1.0-self.smooted)*(alt_Value))
                                     self.alt_W_S = 3
-                                elif  not (10000 <= alt_Value <= 12500):
-                                    self.alt = int(runningMean(alt_Value, 3))
+                                elif  not (10000 <= alt_Value >= 12500):
+                                    self.alt = int((self.alt*self.smooted)+(1.0-self.smooted)*(alt_Value))
                                     self.alt_W_S = 1
                                 else:
                                     self.alt = 0
                                     self.alt_W_S = 2
-                            except ValueError:
+                            except:
                                 pass
-                                #print self.alt
-                        elif dataDict["sensor"] == "MiCS-5525":
+                            #print self.alt
+                        elif dataDict["name"] == "Carbon_Monoxide":
+                            try:
                                 self.co = i.getVal()
-                        elif dataDict["sensor"] == "Voltmeter":
+                                if self.co < 1000:
+                                    self.co_W_S = 3
+                                else:
+                                    self.co = 1000
+                                    self.co_W_S = 2
+                            except:
+                                pass
+                        elif dataDict["name"] == "Volt":
                                 self.volt = i.getVal()
-                        elif dataDict["name"] == "Temperature":
+                                #print self.volt
+                        elif dataDict["name"] == "CAT":
                             cat_sample += 1
-                            if cat_sample >= 3:
+                            if cat_sample >= 5:
                                 try:
                                     self.cat = i.getVal()
                                     cat_sample = 0
@@ -165,18 +252,31 @@ class GPIO_Process(threading.Thread):
                                         self.cat_W_S = 3
                                     else:
                                         self.cat_W_S = 2
-                                except ValueError:
+                                except:
                                     pass
-                        elif dataDict["sensor"] == "10k-pot":
+                            #print self.cat
+                        elif dataDict["name"] == "hg":
                                 self.hg = i.getVal()
+                        elif dataDict["name"] == "OAT":
+                            oat_sample += 1
+                            if oat_sample >= 5:
+                                try:
+                                    self.oat = i.getVal()
+                                    oat_sample = 0
+                                    if self.oat > -50:
+                                        self.oat_W_S = 3
+                                    else:
+                                        self.oat_W_S = 2
+                                except:
+                                    pass
+                            #print self.oat
                         data.append(dataDict)
                 working = True
                 if working != True:
                         print "Failed to upload"
                         
-                time.sleep(.2)
-                self.Panel_anouciator = 'Null'
-                data = [self.speed, self.pitch, self.roll, self.heading, self.alt, self.co, self.volt, self.cat, self.hg, self.alt_W_S, self.cat_W_S, self.gyro_W_S, self.mag_W_S, self.ias_W_S, self.oat_W_S]
+                time.sleep(.4)
+                data = [self.ias, self.pitch, self.roll, self.heading, self.alt, self.co, self.volt, self.cat, self.hg, self.oat, self.ias_W_S, self.gyro_W_S, self.heading_W_S, self.alt_W_S, self.oat_W_S, self.cat_W_S]
                 data_test = str(data).strip('[]')
                 self.queue.put(data_test)
 
@@ -191,3 +291,7 @@ if __name__ == '__main__':
     t = GPIO_Process(q)
     t.start()
     t.join()
+    q2 = queue.Queue()
+    t2 = VW(q2)
+    t2.start()
+    t2.join()
